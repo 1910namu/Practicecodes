1fee32518961b4f4e120037dd5fe2eeaf9c29a40
1910namu <89509767+1910namu@users.noreply.github.com>
2023-11-08 17:24:08 +0530

    Add files via upload

---
+++ block.java
@@ -0,0 +1,21 @@
+package classobject;
+
+public class block {
+	
+	public block(){
+		System.out.println("Hello");
+	}
+
+	{
+		int a=10;
+		int b= 20;
+		System.out.println(a+b);
+	
+	
+	}
+	public static void main(String[] args) {
+		
+		block c=new block();
+	}
+
+}
